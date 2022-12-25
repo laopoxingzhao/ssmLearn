30fda1d912fd31646e1440d4b9daefd7c3a7c6b1
hei <78848316+laopoxingzhao@users.noreply.github.com>
2022-12-25 15:36:30 +0800

    Add files via upload

---
+++ ssm/mybatis/src/main/java/com/hu/Util/SqlSessionUtil.java
@@ -0,0 +1,24 @@
+package com.hu.Util;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SqlSessionUtil {
+    public  static SqlSession getSqlSession(){
+        InputStream stream = null;
+        try {
+            stream = Resources.getResourceAsStream("mybatis-config.xml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory factory =  builder.build(stream);
+        SqlSession session = factory.openSession(true);//会自动提交
+        return session;
+    }
+}
