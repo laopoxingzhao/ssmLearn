01d366fb2e10ec8319b6cc88968b507cba45b4a4
hei <78848316+laopoxingzhao@users.noreply.github.com>
2022-12-28 22:47:05 +0800

    Delete ssm directory

--- ssm/SpringMvc/src/main/java/com/hu/springmvc/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.hu.springmvc.service;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class UserService {
-}
--- ssm/SpringMvc/src/main/java/com/hu/springmvc/user.java
+++
@@ -1,9 +0,0 @@
-package com.hu.springmvc;
-
-public class user {
-    String name;
-
-    public user(String name) {
-        this.name = name;
-    }
-}
